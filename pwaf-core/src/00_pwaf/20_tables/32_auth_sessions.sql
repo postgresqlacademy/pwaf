db3f7fe8246073cb7500bf2e6096be259ef33cbe
Karolis Strumskis <karolis@strumskis.com>
2014-12-29 22:07:54 +0200

    Session Init performance problem fix
    Added index to sessions table, cleanup of old session data.

--- pwaf-core/src/00_pwaf/20_tables/32_auth_sessions.sql
+++ pwaf-core/src/00_pwaf/20_tables/32_auth_sessions.sql
@@ -28,4 +28,38 @@ $body$
 ;
 
 ALTER TABLE pwaf.auth_sessions OWNER TO pwaf;
+
+--
+
+DROP INDEX pwaf.auth_sessions_ip_address_cookie_idx;
+
+CREATE INDEX auth_sessions_ip_address_cookie_idx
+  ON pwaf.auth_sessions
+  USING btree
+  (ip_address, cookie COLLATE pg_catalog."default");
+
+
+--
+DO
+$body$
+BEGIN
+	IF NOT EXISTS (
+		SELECT 1 FROM information_schema.columns WHERE table_catalog=current_database() and table_schema='pwaf' and table_name='auth_sessions' 
+		and column_name='last_activity_date'
+	) THEN
+
+		ALTER TABLE pwaf.auth_sessions ADD COLUMN last_activity_date date;
+		ALTER TABLE pwaf.auth_sessions ALTER COLUMN last_activity_date SET DEFAULT now();
+
+	END IF;
+END
+$body$
+;
+
+DROP INDEX pwaf.auth_sessions_last_activity_date_idx;
+
+CREATE INDEX auth_sessions_last_activity_date_idx
+  ON pwaf.auth_sessions
+  USING btree
+  (last_activity_date);
 --
--- pwaf-core/src/00_pwaf/40_functions/10_sys_session_init.sql
+++ pwaf-core/src/00_pwaf/40_functions/10_sys_session_init.sql
@@ -16,6 +16,9 @@ DECLARE
 	v_cookie text;
 BEGIN
 
+	-- @TODO: implement better old session cleaning
+	DELETE FROM pwaf.auth_sessions WHERE last_activity_date < (NOW() - INTERVAL '3 days')::date;
+
 	IF ((pwaf.pub_http_request_system_param_get(in_request,'session_id') IS NOT NULL) AND (pwaf.pub_http_request_system_param_get(in_request,'Remote-Addr') IS NOT NULL)) THEN
 	
 		v_remote_addr := pwaf.pub_http_request_system_param_get(in_request,'Remote-Addr')::inet;
@@ -24,9 +27,9 @@ BEGIN
 		SELECT id FROM pwaf.auth_sessions WHERE ip_address = v_remote_addr AND cookie = v_cookie AND last_activity > ( now() - INTERVAL '1 hour' )::timestamp with time zone ORDER BY id DESC LIMIT 1 INTO v_session_id;
 
 		IF v_session_id IS NULL THEN
-			INSERT INTO pwaf.auth_sessions (ip_address, cookie, last_activity) VALUES (v_remote_addr,v_cookie, now()) RETURNING id INTO v_session_id;
+			INSERT INTO pwaf.auth_sessions (ip_address, cookie, last_activity, last_activity_date) VALUES (v_remote_addr,v_cookie, now(), now()) RETURNING id INTO v_session_id;
 		ELSE
-			UPDATE pwaf.auth_sessions SET last_activity=now() WHERE id=v_session_id;
+			UPDATE pwaf.auth_sessions SET last_activity=now(), last_activity_date=now() WHERE id=v_session_id;
 		END IF;
 
 	END IF;
